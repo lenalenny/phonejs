845c93e23de7ba146e89cce9b4ebd83b22bbd0b5
Elena Krasnoryadtseva <krasnoryadtsevalena@gmail.com>
2017-08-30 10:03:42 +0300

    babel-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,19 @@ module.exports = {
       { 
       	test: /\.hbs/, 
       	loader: "handlebars-loader" 
+      },
+
+      {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+          plugins: ['transform-runtime'],
+        }
       }
+    }
     ]
   }
 };
