62821aefa3bc324340c1afe31b1f3fa00cb538d9
Elena Krasnoryadtseva <krasnoryadtsevalena@gmail.com>
2017-08-30 08:44:58 +0300

    initial commit

---
+++ frontend/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import PhonePage from './components/phone-page/phone-page.js';
+
+new PhonePage({
+  el: document.querySelector('[data-component="phone-page"]')
+});
---
+++ js/phone-catalogue.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class PhoneCatalogue {
+  constructor(options) {
+    this._el = options.el;
+    this._phones = options.phones;
+
+    const template = document.getElementById('phone-catalogue-template').innerHTML;
+
+    this._compiledTemplate = _.template(template);
+
+    this._render();
+  }
+
+  _render() {
+    this._el.innerHTML = this._compiledTemplate({
+      phones: this._phones
+    });
+  }
+}
